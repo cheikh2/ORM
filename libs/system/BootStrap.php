f3986ba3de2dd559dd528632d32edc3a4ea3c6f0
Cheikh Mbow <cheikh2mbow@gmail.com>
2020-07-13 01:53:46 +0000

    fife_commit

--- libs/system/BootStrap.php
+++ libs/system/BootStrap.php
@@ -4,6 +4,15 @@ namespace libs\system;
 class BootStrap{
     public function __construct()
     {
-        
+      
+      ######---si l'utilisateur tape quelque chose au niveau du navigateur----#########
+      if(isset($_GET["url"]))
+      {
+        echo $_GET["url"];
+      }  
+      else
+      {
+          echo "MVC";
+      }
     }
 }
